c4592335eafc480bfdf476576c82e02f2321c2fa
hrodmn <henry.rodman@gmail.com>
2025-06-26 20:50:14 -0500

    install curl, then uv without conda

--- build-env.sh
+++ build-env.sh
@@ -2,8 +2,14 @@
 set -euo pipefail
 # This script is used to install any custom packages required by the algorithm.
 
+# install curl if not available
+command -v curl >/dev/null 2>&1 || { 
+    echo "Installing curl..."
+    conda install curl
+}
+
 # install uv
-conda install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
 # Get current location of build script
